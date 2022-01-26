08aba8e73a09f661b698e4588c55fbdf0ce7181d
AZERTY44 <enoal.fauchille@gmail.com>
2022-01-26 23:11:50 +0100

    commit

---
+++ commands/AzerDev/addbot.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const fetch = require('node-fetch');
+
+module.exports = {
+    name: 'addbot',
+    description: "Ajoute un bot sur AzerDev",
+    execute(message, args) {
+        
+    }
+}
